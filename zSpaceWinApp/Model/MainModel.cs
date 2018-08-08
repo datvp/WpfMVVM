0b86c79911d3999963f928370e65e218523dd788
Dat Vo <dat.vo@terralogic.com>
2018-08-08 14:24:28 +0700

    init app

---
+++ zSpaceWinApp/Model/MainModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zSpaceWinApp.Model
+{
+    public class MainModel: BaseModel
+    {
+        private string _Title = "";
+        public string Title {
+            get
+            {
+                return _Title;
+            }
+
+            set
+            {
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
